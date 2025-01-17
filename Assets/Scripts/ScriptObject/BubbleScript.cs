a83caf9bf026676b7f14500d37fe7d5be6490520
lin081511 <3031772695@qq.com>
2025-01-17 22:14:58 +0800

    脚本化对象

---
+++ Assets/Scripts/ScriptObject/BubbleScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(
+    menuName = "Bubble/BubbleScript",
+    fileName = "BubbleScript",
+    order = 0)]
+public class BubbleScript : ScriptableObject
+{
+    public int id;
+    public float health;
+
+}
---
+++ Assets/Scripts/ScriptObject/StudentScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(
+    menuName = "Student/StudentScript",
+    fileName = "StudentScript",
+    order = 0)]
+public class StudentScript : ScriptableObject
+{
+    public int id;
+    public float health;
+
+}
