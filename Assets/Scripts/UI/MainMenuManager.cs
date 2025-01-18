65352c9cdb50cad8ed5756238ae2452bb77b1122
卓泽 <wisteria_villosa_rehder@outlook.com>
2025-01-18 11:47:21 +0800

    part of UI 3

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+    {
+    /// <summary>
+    /// 读取场景呗
+    /// </summary>
+    /// <param name="sceneID"></param>
+    public void LoadScene(int sceneID)
+        {
+        SceneManager.LoadScene(sceneID);
+        Debug.Log("场景切换成功");
+        }
+    /// <summary>
+    /// 退出游戏呗
+    /// </summary>
+    public void ExitGame()
+        {
+        Application.Quit();
+        }
+
+    }
