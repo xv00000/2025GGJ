0eb4288b553466ef9319b36441d4e68511ba876b
MilesHunter <956726840@qq.com>
2025-01-19 10:22:15 +0800

    部分修改
    1. 修改了一下部分错误的美术资产的位置
    2. 新增了背景日期修改（DateChange.cs）

---
+++ Assets/Scripts/ProcessManager/DateChange.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DateChange : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshPro;
+    private int[] date = { 99, 82, 64, 42, 31, 17, 7 };
+    public void ChangeDate(int stage)
+    {
+        int dateValue = date[stage];
+
+        // 修改TextMeshPro组件的文本内容
+        if (dateValue > 9)
+        {
+            textMeshPro.text = dateValue.ToString();
+        }
+        else
+        {
+            textMeshPro.text = "0"+dateValue.ToString();
+        }
+    }
+}
