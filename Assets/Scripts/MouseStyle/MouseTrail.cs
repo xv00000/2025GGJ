7180da0c31233087b059dee0eab35da1dff228a9
MilesHunter <956726840@qq.com>
2025-01-19 13:06:42 +0800

    鼠标拖尾

---
+++ Assets/Scripts/MouseStyle/MouseTrail.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTrail : MonoBehaviour
+{
+    public GameObject trailObject;
+    public bool isEffective = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trailObject.GetComponent<TrailRenderer>().enabled = isEffective;
+    }
+
+    // Update is called once per frame
+    public void TrailOn()
+    {
+        isEffective = !(isEffective);
+        trailObject.GetComponent<TrailRenderer>().enabled = isEffective;
+
+    }
+
+    public void DrayMouse()
+    {
+        transform.position = Input.mousePosition;
+    }
+}
