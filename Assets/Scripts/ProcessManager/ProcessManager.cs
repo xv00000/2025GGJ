5acd5d94870cdb55cc8848312efb3215a501e7eb
lin081511 <3031772695@qq.com>
2025-01-19 11:39:54 +0800

    自动开技能

--- Assets/Scripts/ProcessManager/ProcessManager.cs
+++ Assets/Scripts/ProcessManager/ProcessManager.cs
@@ -23,9 +23,11 @@ public static class Data
     }
 public class ProcessManager : MonoBehaviour
     {
+    bool craze = false;
     float time = 0;
     [SerializeField] bool randomGenerate;
     [SerializeField] float interval;
+    [SerializeField] float crazeTime = 0.5f;
     bool a = false;
     float aimFill;
     float length = 20;
@@ -225,6 +227,8 @@ public class ProcessManager : MonoBehaviour
             string type = line[6].ToString() + line[7].ToString();
             float nextTime = (line[9] - 48) * 100 + (line[10] - 48) * 10 + (line[11] - 48) * 1f + (line[12] - 48) * 0.1f + (line[13] - 48) * 0.01f + (line[14] - 48) * 0.0011f;//(line[9] - 48) * 10 + line[10] - 48 + (line[11] - 48) * 0.1f;
             Debug.Log(id + " " + studentId + " " + type + " " + nextTime);
+            if (nextTime < crazeTime && !craze) { ReflectionManager.Instance.Reflect("进入狂暴模式！！！", Vector3.zero, Color.red, 80);craze = true;Skill._1 = true; }
+            else if (nextTime >= crazeTime && craze) { craze = false; Skill._1 = false; }
             switch (type)
                 {
                 case "01": GenerateBubble(FindBubble(id), studentId); break;
